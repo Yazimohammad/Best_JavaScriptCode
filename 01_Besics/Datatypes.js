a5af0b7166adb42c3512a38c052dd3211b4fb09d
Mohammad Yazdan <114860029+Yazimohammad@users.noreply.github.com>
2023-08-26 18:35:44 +0000

    Second comit

---
+++ 01_Besics/Datatypes.js
@@ -0,0 +1,13 @@
+const accountId = '123456678';
+const accountName = "Yazdan";
+const accountEmail = "yazi@gmail.com"
+const accountpassword = '12121212';
+IsLogin = true;
+accountCity = "pune";
+// console.log(accountId);
+// console.log(accountName);
+// console.log(accountpassword);
+// console.log(accountCity);
+// console.log(accountEmail);
+
+console.table([accountId , accountName,accountpassword , accountEmail, accountCity])
--- 01_Besics/test.js
+++ 01_Besics/test.js
@@ -1,5 +1,6 @@
+
+console.log ("yazdan")
 var a = 5 ;
 var b = 10 ;
-
 var c = a+b;
 console.log(c);
