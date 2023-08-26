469468c14ad6191c79e4d2ae6212307462ba8725
Mohammad Yazdan <114860029+Yazimohammad@users.noreply.github.com>
2023-08-26 19:05:58 +0000

    conversation of datatypes

---
+++ 01_Besics/conversionOpration.js
@@ -0,0 +1,32 @@
+let score = "33abc"
+// let score = null;
+// let score = undefine;
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);
+
+// if we take "33"  it will convert => 33
+//if we take "33acb"  it will convert => NaN
+//  if we take true it will convert => 1  and fales convert in 0 ;
+
+let IsLogined = " yazi" ;
+let booleanIslogined = Boolean(IsLogined);
+// console.log (booleanIslogined)
+
+// in boolean convert 1 => ture
+// in boolean convert "" => false
+// in boolean convert "yazi" => true
+
+let someNumber = 25 
+
+let stringNumber = String(someNumber)
+// console.log (stringNumber);
+// console.log (typeof (stringNumber))
+
+let data = 'Mumbai'
+let res = Number(data)
+console.log( typeof (res))
